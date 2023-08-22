e5efd7a3d14c083efdb5789aa3930e0815367f08
Anthony Nzau <nzauanthony7@gmail.com>
2023-08-22 08:32:05 -0700

    concat edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[:6], end=' ')
-print(str[6:14], end=' ')
-print(str[15:38], end=' ')
-print(str[39:43], end=' ')
-print(str[44:], end='\n')
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
