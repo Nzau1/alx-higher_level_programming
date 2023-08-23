b613d5d12b368452996c0759ee65a81580bb59e9
Antony Nzau <nzauanthony7@gmail.com>
2023-08-23 11:14:09 +0000

    the last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+# Get the last digit of the number using modulo operator
+last_digit = abs(number) % 10  # Take absolute value to handle negative numbers
+
+# Determine the appropriate message based on the last digit
+message = "The string Last digit of {} is {} and is ".format(number, last_digit)
+
+if last_digit > 5:
+    message += "greater than 5"
+elif last_digit == 0:
+    message += "0"
+else:
+    message += "less than 6 and not 0"
+
+# Print the message
+print(message)
