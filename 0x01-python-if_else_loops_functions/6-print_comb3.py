12a129fa99000398a95ff11eae271b22f7041f00
Antony Nzau <nzauanthony7@gmail.com>
2023-08-23 12:31:01 +0000

    the more brains you use the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " if ones_digit < 9 else "\n")
+
+"""Print all possible different combinations of two digits in ascending order.
+
+    The two digits must be different - 01 and 10 are considered identical.
+    """
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:02d}, {:02d}".format(i, j), end=", " if j < 9 else "")
